6c796d83c6e203176db7bf5deaccc9be03200c2b
DhanvantG <dhanvantg@gmail.com>
2025-02-26 01:43:55 +0530

    Added JSON report flag

--- xl.go
+++ xl.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"math"
+	"os"
 	"strconv"
-
-	//"encoding/json"
-	"flag"
 
 	"github.com/xuri/excelize/v2"
 )
+
+type Report struct {
+	Averages           map[string]float64    `json:"Averages"`
+	BranchWiseAverages map[string]float64    `json:"BranchWiseAverages"`
+	Top3               map[string][]TopScore `json:"Top3"`
+	Discrepancies      map[string]string     `json:"Discrepancies"`
+}
+
+type TopScore struct {
+	Rank  int     `json:"Rank"`
+	ID    string  `json:"ID"`
+	Score float64 `json:"Score"`
+}
 
 func main() {
 	pathFlag := flag.String("path", "", "Path to the file")
 	jsonFlag := flag.String("export", "", "Export to JSON")
 	classFlag := flag.String("class", "", "Class to filter")
 	flag.Parse()
-	fmt.Println(*jsonFlag)
 	if *pathFlag == "" {
 		fmt.Println("Please provide the file path")
 		return
@@ -53,7 +65,12 @@ func main() {
 	branch := ""
 	emplid := ""
 	headers := rows[0]
-	//fmt.Println("Headers:", headers)
+
+	// Report Variables
+	total_avs := map[string]float64{}
+	branch_avs := map[string]float64{}
+	top3report := map[string][]TopScore{}
+	discrepancy := map[string]string{}
 
 	for _, row := range rows[1:] {
 		if len(row) == 0 {
@@ -95,6 +112,7 @@ func main() {
 				if sum != i {
 					fmt.Println("Discrepancy Found! Total not matching")
 					fmt.Println("For", emplid, "Expected total to be", i, "but turned out to be", sum)
+					discrepancy[emplid] = fmt.Sprintf("Expected total to be %f but turned out to be %f", i, sum)
 				}
 				total += sum
 				count++
@@ -104,29 +122,65 @@ func main() {
 				sum = 0
 			}
 		}
-		//fmt.Println()
+	}
+	if count == 0 {
+		fmt.Println("No data found, please check the flags")
+		return
 	}
 	fmt.Println("Averages:")
 	fmt.Println("Total: ", total/float64(count))
-
+	total_avs["Total"] = total / float64(count)
 	// Component wise avs
 	for i, v := range exams {
 		fmt.Println(headers[i], ":", v/float64(count))
+		total_avs[headers[i]] = v / float64(count)
 	}
 
 	// Branch wise avs
 	fmt.Println("24 Batch Single Degree Branch wise averages:")
 	for i, v := range branches {
 		fmt.Println(i, ":", v/float64(branch_count[i]))
+		branch_avs[i] = v / float64(branch_count[i])
 	}
 	for rowNo, header := range headers[4:10] {
 		rowNo += 4
 		top3, top3emplids := gettop3(components[rowNo][0], components[rowNo][1])
 		fmt.Println("Top 3 in", header, ":")
+		top3s := []TopScore{}
+
 		for i, v := range top3 {
 			fmt.Println(i+1, top3emplids[i], ":", v)
+			top3s = append(top3s, TopScore{Rank: i + 1, ID: top3emplids[i], Score: v})
 		}
+		top3report[header] = top3s
 	}
+
+	if *jsonFlag == "" {
+		return
+	}
+	file, err := os.Create("report.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	report := Report{
+		Averages:           total_avs,
+		BranchWiseAverages: branch_avs,
+		Top3:               top3report,
+		Discrepancies:      discrepancy,
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+	err = encoder.Encode(report)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+
+	fmt.Println("Report exported to report.json")
 }
 
 func gettop3(marks []interface{}, emplids []interface{}) ([]float64, []string) {
